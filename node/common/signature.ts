4521398967d1eef98f1b585d55e3f83498ec4e15
Plenty Su <plenty.su@gmail.com>
2022-09-09 01:05:40 +0900

    Add a new interface: SignatureValidator

--- node/common/signature.ts
+++ node/common/signature.ts
@@ -1,3 +1,7 @@
 export interface SignatureSigner {
   sign(data: Uint8Array): Promise<Uint8Array>;
 }
+
+export interface SignatureValidator {
+  validate(data: Uint8Array, signature: Uint8Array): Promise<boolean>;
+}
