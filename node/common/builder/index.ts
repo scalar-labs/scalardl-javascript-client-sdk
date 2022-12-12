3f24b9f385de3500b499febda7650aeb615768f2
Plenty Su <plenty.su@gmail.com>
2022-12-12 18:00:13 +0900

    Add the builder for contract listing requests

--- node/common/builder/index.ts
+++ node/common/builder/index.ts
@@ -1,3 +1,4 @@
 export * from './certificate_registration_request';
 export * from './contract_registration_request';
 export * from './function_registration_request';
+export * from './contract_listing_request';
