e77bab060ad2451932e94ce347cd1de990a1a381
Plenty Su <plenty.su@gmail.com>
2022-12-14 13:31:27 +0900

    Avoid using jsrsasign for excoding

--- node/common/polyfill/text_encoder.ts
+++ node/common/polyfill/text_encoder.ts
@@ -1,5 +1,3 @@
-import * as jsrsasign from 'jsrsasign';
-
 /**
  * An internal class to encode utf8 string to Uint8Array
  * @class
@@ -11,10 +9,48 @@ export class TextEncoder {
    * @return {Uint8Array}
    */
   encode(encoding: string): Uint8Array {
-    return !encoding
-      ? new Uint8Array()
-      : new Uint8Array(
-          jsrsasign.hextoArrayBuffer(jsrsasign.utf8tohex(encoding))
-        );
+    const hex = encodeURIComponentAll(encoding)
+      .replace(/%/g, '')
+      .toLocaleLowerCase();
+    const buffer = new ArrayBuffer(hex.length / 2);
+    const view = new DataView(buffer);
+
+    for (let i = 0; i < hex.length / 2; i++) {
+      view.setUint8(i, parseInt(hex.substring(i * 2, i * 2 + 2), 16));
+    }
+
+    return new Uint8Array(buffer);
   }
+}
+
+function encodeURIComponentAll(u8: string) {
+  const s = encodeURIComponent(u8);
+  let s2 = '';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '%') {
+      s2 = s2 + s.substring(i, i + 3);
+      i = i + 2;
+    } else {
+      // A-Z a-z 0-9 - _ . ! ~ * ' ( )
+      s2 = s2 + '%' + stohex(s[i]);
+    }
+  }
+  return s2;
+}
+
+function stohex(s: string) {
+  const ba = [];
+  for (let i = 0; i < s.length; i++) {
+    ba[i] = s.charCodeAt(i);
+  }
+
+  let hex = '';
+
+  for (let i = 0; i < ba.length; i++) {
+    let hex1 = ba[i].toString(16);
+    if (hex1.length === 1) hex1 = '0' + hex1;
+    hex = hex + hex1;
+  }
+
+  return hex;
 }
