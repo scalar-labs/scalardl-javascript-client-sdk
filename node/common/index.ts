baa80904c1dece55efbda30003b519053b74a295
Plenty Su <plenty.su@gmail.com>
2023-02-01 17:07:39 +0900

    Export all necessary definitions of the `common` part (#24)

---
+++ node/common/index.ts
@@ -0,0 +1,7 @@
+export {ClientServiceBase} from './client_service_base';
+export {ClientError} from './client_error';
+export {StatusCode} from './status_code';
+export {ContractExecutionResult} from './contract_execution_result';
+export {LedgerValidationResult} from './ledger_validation_result';
+export {AssetProof} from './asset_proof';
+export {ClientConfig, CLIENT_PROPERTIES_FIELD} from './client_config';
