769bbd537ad81fa962d30dabbb4feda5917f60ca
Plenty Su <plenty.su@gmail.com>
2023-04-24 11:21:44 +0900

    End-to-end test for Web (#33)

--- web/index.ts
+++ web/index.ts
@@ -1,2 +1,7 @@
 export {ClientService} from './client_service';
 export {SignerFactory} from './signer';
+export {StatusCode} from './common/status_code';
+export {ContractExecutionResult} from './common/contract_execution_result';
+export {LedgerValidationResult} from './common/ledger_validation_result';
+export {AssetProof} from './common/asset_proof';
+export {ClientError} from './common/client_error';
